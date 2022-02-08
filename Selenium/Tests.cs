f5bbb749d3d03721f0f2b4a05ce7bb7b91b8f31a
Bryce Crist <brucecrusty@gmail.com>
2022-02-07 18:05:24 -0700

    Initial commit

---
+++ Selenium/Tests.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+namespace Selenium
+{
+    [TestFixture]
+    public class Tests
+    {
+        [Test]
+        public void Test1()
+        {
+            IWebDriver driver = new ChromeDriver();
+            driver.Navigate().GoToUrl("https://www.google.com");
+            Assert.True(true);
+            driver.Close();
+        }
+    }
+}
